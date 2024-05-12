ab61815147ff6f6cfe011412e70c2fcce0720df2
Денис Котелев <deniskotelev@MacBook-Pro-Denis.local>
2024-05-12 20:27:38 +0300

    f

---
+++ src/main.ts
@@ -0,0 +1,27 @@
+import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const configService = app.get(ConfigService);
+
+  const urlClient = configService.get<string>('URL_CLIENT');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+  app.setGlobalPrefix('api');
+  app.enableCors({
+    origin: [urlClient],
+    methods: 'GET, HEAD, PUT, PATCH, POST, DELETE',
+    credentials: true,
+  });
+
+  await app.listen(8080);
+}
+bootstrap();
