81abb71010636b433126bf56e2df57714bbd752f
Денис Котелев <deniskotelev@MacBook-Pro-Denis.local>
2024-07-02 22:24:46 +0300

    f

--- src/tonapi/tonapi.service.ts
+++ src/tonapi/tonapi.service.ts
@@ -1,17 +1,39 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { lastValueFrom, map } from 'rxjs';
+
+interface EnvironmentVariables {
+  NFT_COLLECTION_ADDRESS: string;
+  TONAPI_TOKEN: string;
+}
 
 @Injectable()
 export class TonapiService {
-  constructor(private readonly httpService: HttpService) {}
+  constructor(
+    private readonly httpService: HttpService,
+    private configService: ConfigService<EnvironmentVariables>,
+  ) {}
 
   async getNftByAddress(address: string) {
+    const collectionAddress = this.configService.get('NFT_COLLECTION_ADDRESS');
+    const topapiToken = this.configService.get('TONAPI_TOKEN');
+
     try {
       const data = await lastValueFrom(
         this.httpService
           .get<{ nft_items: Array<object> }>(
             `https://tonapi.io/v2/accounts/${address}/nfts`,
+            {
+              params: {
+                collection: collectionAddress,
+              },
+              headers: {
+                Authorization: topapiToken
+                  ? `Bearer ${topapiToken}`
+                  : undefined,
+              },
+            },
           )
           .pipe(map((res) => res.data)),
       );
